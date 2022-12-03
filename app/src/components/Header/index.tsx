7f0f54df57a91a62ed014e9b28abb647eac7111e
Felipe Marques <felipe.marqs@outlook.com>
2022-12-03 09:37:00 -0300

    Update index.tsx

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ export function Header() {
 		<Container>
 			<Text size={14} opacity={0.9}> Bem Vindo(a) ao </Text>
 			<Text size={24} weight="700"> 
-				WAITER
+				IASMIN
 				<Text size={24}>APP</Text> 
 			</Text>
 		</Container>
